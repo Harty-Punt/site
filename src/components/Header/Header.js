131865d63a7e3a1b293c3c1e9db703501062633b
Guilherme Vieira <glvs011@gmail.com>
2021-01-06 01:30:43 -0300

    feat(header and about): finish header and create about structure component.

---
+++ src/components/Header/Header.js
@@ -0,0 +1,17 @@
+
+
+export const Header = () => {
+    return (
+        <>
+            <nav class="navbar-default">
+                <ul>
+                    <li><a href="/">Início</a></li>
+                    <li><a href="#about">A guild</a></li>
+                    <li><a href="#applications">Aplicações</a></li>
+                    <li><a href="/">Harty Punt Awards</a></li>
+                    <li><a href="/">Transparência HP</a></li>
+                </ul>
+            </nav>
+        </>
+    )
+}
